27bcfb2d588b8a090d37122299945564e3a6f40b
FruitGus <FruitGus@yandex.ru>
2026-02-20 13:06:35 +0500

    create first migration for hotels

--- src/database.py
+++ src/database.py
@@ -1,4 +1,6 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
 
 from src.config import settings
 
@@ -6,3 +8,10 @@ from src.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL)
 
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+
+
+class Base(DeclarativeBase):
+    pass
