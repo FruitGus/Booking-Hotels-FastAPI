88726939f49d9752a214dbf6a9a105170e44638d
FruitGus <FruitGus@yandex.ru>
2026-02-23 20:55:30 +0500

    add print to post.hotels

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -4,7 +4,7 @@ from fastapi import Query, APIRouter, Body
 from sqlalchemy import insert
 
 from src.api.dependencies import PaginationDep
-from src.database import async_session_maker
+from src.database import async_session_maker, engine
 from src.models.hotels import HotelsOrm
 from src.schemas.hotels import Hotel, HotelPATCH
 
@@ -60,6 +60,7 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
 ):
     async with async_session_maker() as session:
         add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        print(add_hotel_stmt.compile(engine, comple_kwargs={"literal_binds": True}))
         await session.execute(add_hotel_stmt)
         await session.commit()
 
