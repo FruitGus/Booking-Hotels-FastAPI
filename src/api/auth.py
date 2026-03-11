a9934b92bd44c45802efaed06d09dbc8b06be6ad
FruitGus <FruitGus@yandex.ru>
2026-03-11 13:35:25 +0500

    create jwt token

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from datetime import timedelta, datetime, timezone
+
+from fastapi import APIRouter, HTTPException, Response
 
 from passlib.context import CryptContext
+import jwt
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
@@ -11,6 +14,23 @@ router = APIRouter(prefix="/auth", tags=["Авторизация и аутент
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = "2414321014SAdfasfa2414413214"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
+
 
 
 
@@ -25,3 +45,16 @@ async def register_user(
         await session.commit()
 
     return {"status": "OK"}
+
+
+@router.post("/login")
+async def login_user(data: UserRequestAdd, response: Response):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегестрирован")
+        if not verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Пароль неверный")
+        access_token = create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+        return {"access_token": access_token}
